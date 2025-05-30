5049237e0c612c3f6eb557b49c3a08bdd9e8526c
Madesh035 <mightymadesh035@gmail.com>
2025-05-30 09:00:26 +0000

    first

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server:{
+    allowedHosts:["5173-elango3725-cms-project-vp8mjl4k4k.app.codeanywhere.com"]
+  },
   plugins: [react()],
 })
